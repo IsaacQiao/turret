1a980e093a948c099ec6f8d5c678b7dfd7597990
IsaacQiao <bqiao@sfu.ca>
2018-08-13 01:00:15 +0000

    work now

--- facerec.py
+++ facerec.py
@@ -132,7 +132,7 @@ while True:
     # Find all the faces and face encodings in the current frame of video
     '''Thread for facial recongition'''
     face_thread = threading.Thread(target=face_detection_sec, args=(output, firebase, faceCascade, logger))
-    turret_thread = threading.Thread(target=t.motion_detection_rasp, args=(output))
+    turret_thread = threading.Thread(target=t.motion_detection_rasp, args=(output,))
     
     face_thread.start()
     turret_thread.start()
